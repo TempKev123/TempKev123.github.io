5ab2e037c58c64f42b4f1e84aa62a3cf4fdd92d1
Sujit "Kevin" Dityam <u6511800@au.edu>
2024-08-25 12:06:58 +0700

    changed paths to work better

--- project-car/App.jsx
+++ project-car/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
-import Dashboard  from './pages/Dashboard';
-import HighlightedCars from './pages/HighlightedCars';
-import CarAnalysis from './pages/CarAnalysis'; // Import CarAnalysis
+import Dashboard  from './pages/Dashboard'; //WE, ♪ MESSED UP THE NAMES ♪
+import CarAnalysis from './pages/CarAnalysis';
 import './App.css'; // Optional: for styling the header
 
 const App = () => (
@@ -12,8 +11,7 @@ const App = () => (
     </header>
     <main>
       <Routes>
-        <Route path="/" element={<HighlightedCars />} />
-        <Route path="/car-analysis" element={<CarAnalysis />} />
+        <Route path="/" element={<CarAnalysis/>} />
         <Route path="/highlighted" element={<Dashboard />} />
       </Routes>
     </main>
